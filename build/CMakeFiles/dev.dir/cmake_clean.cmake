8493f07d684d7ce88668f0809cc55321c92cbd43
bhaskar_k <bhaskar.k@myelinfoundry.com>
2025-01-30 14:29:48 +0530

    added callback class

---
+++ build/CMakeFiles/dev.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dev.dir/dev.c.o"
+  "CMakeFiles/dev.dir/dev.c.o.d"
+  "CMakeFiles/dev.dir/xdg-shell-client-protocol.c.o"
+  "CMakeFiles/dev.dir/xdg-shell-client-protocol.c.o.d"
+  "dev"
+  "dev.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
